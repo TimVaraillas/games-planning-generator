d4e099ba6b76a31da681fd95f67f3cd12f84f9c9
Timothée Varaillas <timothee.varaillas@gmail.com>
2019-10-26 18:06:20 +0200

    + Ajout du template Nebular

--- FinalRound-webapp/src/app/app.component.ts
+++ FinalRound-webapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NbSidebarService } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'FinalRound-webapp';
+
+  items = [
+    {
+      title: 'Profile',
+      icon: 'person-outline',
+      link: [],
+    },
+    {
+      title: 'Change Password',
+      icon: 'lock-outline',
+      link: [],
+    },
+    {
+      title: 'Privacy Policy',
+      icon: { icon: 'checkmark-outline', pack: 'eva' },
+      link: [],
+    },
+    {
+      title: 'Logout',
+      icon: 'unlock-outline',
+      link: [],
+    },
+  ];
+
+  constructor(
+    private sidebarService: NbSidebarService
+  ) { }
+
+  toggle() {
+    this.sidebarService.toggle(true);
+    return false;
+  }
 }
--- FinalRound-webapp/src/app/app.module.ts
+++ FinalRound-webapp/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbActionsModule, NbMenuModule, NbIconModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,15 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'dark' }),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbActionsModule,
+    NbEvaIconsModule,
+    NbIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
