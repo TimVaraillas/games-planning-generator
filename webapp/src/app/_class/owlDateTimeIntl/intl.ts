16bf26f0b979ae553b1f0d9ecfaef9bff7272083
Timothée Varaillas <t.varaillas@mobileit.fr>
2019-12-26 15:56:00 +0100

    [ANG] @ Ajout d'une classe de traduction pour le module ng-pick-datetime

---
+++ webapp/src/app/_class/owlDateTimeIntl/intl.ts
@@ -0,0 +1,116 @@
+import { OwlDateTimeIntl } from 'ng-pick-datetime';
+
+export class EnglishIntl extends OwlDateTimeIntl {
+  upSecondLabel = 'up second'; // A label for the up second button (used by screen readers).
+  downSecondLabel = 'down second'; // A label for the down second button (used by screen readers).
+  upMinuteLabel = 'up minute'; // A label for the up minute button (used by screen readers).
+  downMinuteLabel = 'down minute'; // A label for the down minute button (used by screen readers).
+  upHourLabel = 'up hour'; // A label for the up hour button (used by screen readers).
+  downHourLabel = 'down heure'; // A label for the down hour button (used by screen readers).
+  prevMonthLabel = 'previous month'; // A label for the previous month button (used by screen readers).
+  nextMonthLabel = 'next month'; // A label for the next month button (used by screen readers).
+  prevYearLabel = 'previous year'; // A label for the previous year button (used by screen readers).
+  nextYearLabel = 'next year'; // A label for the next year button (used by screen readers).
+  prevMultiYearLabel = 'Previous 21 years'; // A label for the previous multi-year button (used by screen readers).
+  nextMultiYearLabel = 'Next 21 years'; // A label for the next multi-year button (used by screen readers).
+  switchToMonthViewLabel = 'Change to month view'; // A label for the 'switch to month view' button (used by screen readers).
+  switchToMultiYearViewLabel = 'Choose the month and year'; // A label for the 'switch to year view' button (used by screen readers).
+  cancelBtnLabel = 'Cancel'; // A label for the cancel button
+  setBtnLabel = 'Set'; // A label for the set button
+  rangeFromLabel = 'From'; // A label for the range 'from' in picker info
+  rangeToLabel = 'To'; // A label for the range 'to' in picker info
+  hour12AMLabel = 'AM'; // A label for the hour12 button (AM)
+  hour12PMLabel = 'PM'; // A label for the hour12 button (PM)
+}
+
+export class FrenchIntl extends OwlDateTimeIntl {
+  upSecondLabel = 'ajouter une seconde'; // A label for the up second button (used by screen readers).
+  downSecondLabel = 'moins une seconde'; // A label for the down second button (used by screen readers).
+  upMinuteLabel = 'ajouter une minute'; // A label for the up minute button (used by screen readers).
+  downMinuteLabel = 'moins une minute'; // A label for the down minute button (used by screen readers).
+  upHourLabel = 'ajouter une heure'; // A label for the up hour button (used by screen readers).
+  downHourLabel = 'moins une heure'; // A label for the down hour button (used by screen readers).
+  prevMonthLabel = 'le mois précédent'; // A label for the previous month button (used by screen readers).
+  nextMonthLabel = 'le mois prochain'; // A label for the next month button (used by screen readers).
+  prevYearLabel = 'année précédente'; // A label for the previous year button (used by screen readers).
+  nextYearLabel = 'l\'année prochaine'; // A label for the next year button (used by screen readers).
+  prevMultiYearLabel = '21 années précédentes'; // A label for the previous multi-year button (used by screen readers).
+  nextMultiYearLabel = '21 années suivantes'; // A label for the next multi-year button (used by screen readers).
+  switchToMonthViewLabel = 'Passer en vue mensuelle'; // A label for the 'switch to month view' button (used by screen readers).
+  switchToMultiYearViewLabel = 'Choisissez le mois et l\'année'; // A label for the 'switch to year view' button (used by screen readers).
+  cancelBtnLabel = 'Annuler'; // A label for the cancel button
+  setBtnLabel = 'Confirmer'; // A label for the set button
+  rangeFromLabel = 'À partir de'; // A label for the range 'from' in picker info
+  rangeToLabel = 'Jusqu\'à'; // A label for the range 'to' in picker info
+  hour12AMLabel = 'AM'; // A label for the hour12 button (AM)
+  hour12PMLabel = 'PM'; // A label for the hour12 button (PM)
+}
+
+export class GermanIntl extends OwlDateTimeIntl {
+  upSecondLabel = 'up second'; // A label for the up second button (used by screen readers).
+  downSecondLabel = 'down second'; // A label for the down second button (used by screen readers).
+  upMinuteLabel = 'up minute'; // A label for the up minute button (used by screen readers).
+  downMinuteLabel = 'down minute'; // A label for the down minute button (used by screen readers).
+  upHourLabel = 'up hour'; // A label for the up hour button (used by screen readers).
+  downHourLabel = 'down heure'; // A label for the down hour button (used by screen readers).
+  prevMonthLabel = 'Vorheriger Monat'; // A label for the previous month button (used by screen readers).
+  nextMonthLabel = 'Nächster Monat'; // A label for the next month button (used by screen readers).
+  prevYearLabel = 'Vorjahr'; // A label for the previous year button (used by screen readers).
+  nextYearLabel = 'Nächstes Jahr'; // A label for the next year button (used by screen readers).
+  prevMultiYearLabel = 'Vorherige 21 Jahre'; // A label for the previous multi-year button (used by screen readers).
+  nextMultiYearLabel = 'Die nächsten 21 Jahre'; // A label for the next multi-year button (used by screen readers).
+  switchToMonthViewLabel = 'Zur Monatsansicht wechseln'; // A label for the 'switch to month view' button (used by screen readers).
+  switchToMultiYearViewLabel = 'Wählen Sie den Monat und das Jahr aus'; // A label for the 'switch to year view' button (used by screen readers).
+  cancelBtnLabel = 'Abbrechen'; // A label for the cancel button
+  setBtnLabel = 'Bestätigen'; // A label for the set button
+  rangeFromLabel = 'Von'; // A label for the range 'from' in picker info
+  rangeToLabel = 'An'; // A label for the range 'to' in picker info
+  hour12AMLabel = 'AM'; // A label for the hour12 button (AM)
+  hour12PMLabel = 'PM'; // A label for the hour12 button (PM)
+}
+
+export class ItalianIntl extends OwlDateTimeIntl {
+  upSecondLabel = 'up second'; // A label for the up second button (used by screen readers).
+  downSecondLabel = 'down second'; // A label for the down second button (used by screen readers).
+  upMinuteLabel = 'up minute'; // A label for the up minute button (used by screen readers).
+  downMinuteLabel = 'down minute'; // A label for the down minute button (used by screen readers).
+  upHourLabel = 'up hour'; // A label for the up hour button (used by screen readers).
+  downHourLabel = 'down heure'; // A label for the down hour button (used by screen readers).
+  prevMonthLabel = 'Mese precedente'; // A label for the previous month button (used by screen readers).
+  nextMonthLabel = 'Il prossimo mese'; // A label for the next month button (used by screen readers).
+  prevYearLabel = 'Anno precedente'; // A label for the previous year button (used by screen readers).
+  nextYearLabel = 'L\'anno prossimo'; // A label for the next year button (used by screen readers).
+  prevMultiYearLabel = 'Precedente 21 anni'; // A label for the previous multi-year button (used by screen readers).
+  nextMultiYearLabel = 'Prossimi 21 anni'; // A label for the next multi-year button (used by screen readers).
+  switchToMonthViewLabel = 'Passa alla vista mese'; // A label for the 'switch to month view' button (used by screen readers).
+  switchToMultiYearViewLabel = 'Scegli il mese e l\'anno'; // A label for the 'switch to year view' button (used by screen readers).
+  cancelBtnLabel = 'Annulla'; // A label for the cancel button
+  setBtnLabel = 'Conferma'; // A label for the set button
+  rangeFromLabel = 'Da'; // A label for the range 'from' in picker info
+  rangeToLabel = 'A'; // A label for the range 'to' in picker info
+  hour12AMLabel = 'AM'; // A label for the hour12 button (AM)
+  hour12PMLabel = 'PM'; // A label for the hour12 button (PM)
+}
+
+export class SpanishIntl extends OwlDateTimeIntl {
+  upSecondLabel = 'up second'; // A label for the up second button (used by screen readers).
+  downSecondLabel = 'down second'; // A label for the down second button (used by screen readers).
+  upMinuteLabel = 'up minute'; // A label for the up minute button (used by screen readers).
+  downMinuteLabel = 'down minute'; // A label for the down minute button (used by screen readers).
+  upHourLabel = 'up hour'; // A label for the up hour button (used by screen readers).
+  downHourLabel = 'down heure'; // A label for the down hour button (used by screen readers).
+  prevMonthLabel = 'Mes anterior'; // A label for the previous month button (used by screen readers).
+  nextMonthLabel = 'El mes que viene'; // A label for the next month button (used by screen readers).
+  prevYearLabel = 'Ejercicio anterior'; // A label for the previous year button (used by screen readers).
+  nextYearLabel = 'El año que viene'; // A label for the next year button (used by screen readers).
+  prevMultiYearLabel = 'Anteriores 21 años'; // A label for the previous multi-year button (used by screen readers).
+  nextMultiYearLabel = 'Próximos 21 años'; // A label for the next multi-year button (used by screen readers).
+  switchToMonthViewLabel = 'Cambiar a la vista del mes'; // A label for the 'switch to month view' button (used by screen readers).
+  switchToMultiYearViewLabel = 'Seleccione el mes y el año'; // A label for the 'switch to year view' button (used by screen readers).
+  cancelBtnLabel = 'Cancelar'; // A label for the cancel button
+  setBtnLabel = 'Confirmar'; // A label for the set button
+  rangeFromLabel = 'Desde'; // A label for the range 'from' in picker info
+  rangeToLabel = 'Para'; // A label for the range 'to' in picker info
+  hour12AMLabel = 'AM'; // A label for the hour12 button (AM)
+  hour12PMLabel = 'PM'; // A label for the hour12 button (PM)
+}
